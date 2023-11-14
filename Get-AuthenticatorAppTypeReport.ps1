19c90ff889908b6f83c94e40c7f27c5e44355820
Pavel Garmashov <pgarm@pgarm.pro>
2023-11-14 16:07:46 +0000

    Update Get-AuthenticatorAppTypeReport.ps1  (#6)
    * Update Get-AuthenticatorAppTypeReport.ps1
    
    Make readability improvements and better progress reporting for longer runs.
    
    * Update Get-AuthenticatorAppTypeReport.ps1
    
    Make readability improvements and better progress reporting for longer runs.

--- Get-AuthenticatorAppTypeReport.ps1
+++ Get-AuthenticatorAppTypeReport.ps1
@@ -2,41 +2,74 @@
 
 [CmdletBinding()]
 param (
-    [String]$TenantId = '' # Prefill TenantId as GUID of any verified domain name, or supply as parameter
+    [String]$TenantId # Prefill TenantId as GUID of any verified domain name, or supply as parameter
 )
 
 #Helper class for stopwatch used in progress reporting
 class OperationTracker {
     
     OperationTracker([int]$TotalCount) {
+        $this.Init($TotalCount, $false)
+    }
+
+    OperationTracker([int]$TotalCount, [bool]$AutoIncrement) {
+        $this.Init($TotalCount, $AutoIncrement)
+    }
+
+    hidden [void] init([int]$TotalCount, [bool]$AutoIncrement) {
         $this | Add-Member -MemberType ScriptProperty -Name 'TotalCount' -Value { return $TotalCount }.GetNewClosure() -Force
+
+        $this | Add-Member -MemberType ScriptProperty -Name 'Elapsed' -Value {
+            if (!$this.TimeStopped) {
+                return [System.DateTime]::Now - $this.TimeStarted 
+            }
+            else { 
+                return ($this.TimeStopped - $this.TimeStarted) 
+            } 
+        }.GetNewClosure() -Force
+        
+        $this | Add-Member -MemberType ScriptProperty -Name 'PercentComplete' -Value {
+            if ($this.TotalCount -eq 0) { return 0 }
+            return $this.CurrentItem / $this.TotalCount * 100
+        }.GetNewClosure() -Force
+
+        $this | Add-Member -MemberType ScriptProperty -Name 'Remaining' -Value {
+            if ($this.TimeStopped) { return [timespan]::Zero }
+            if ($this.CurrentItem -eq 0) { return [timespan]::Zero }
+            return $this.Elapsed / $this.CurrentItem * ($this.TotalCount - $this.CurrentItem)
+        }.GetNewClosure() -Force
+
+        $this | Add-Member -MemberType ScriptProperty -Name 'Status' -Value {
+            return "$($this.CurrentItem)/$($this.TotalCount)"
+        }.GetNewClosure() -Force
+
+        $this | Add-Member -MemberType ScriptProperty -Name 'AutoIncrement' -Value { return [bool]$AutoIncrement }.GetNewClosure() -Force
+
         $this.Reset()
     }
 
-    [timespan] Elapsed() {
-        if (!$this.TimeStopped) { return [System.DateTime]::Now - $this.TimeStarted } else { return ($this.TimeStopped - $this.TimeStarted) }
+    [hashtable] GetProgress() {
+        if ($this.AutoIncrement) { $this.Next() }
+        return @{
+            PercentComplete  = $this.PercentComplete
+            SecondsRemaining = $this.Remaining.TotalSeconds
+            Status           = $this.Status
+        }
     }
 
-    [float] PercentComplete() {
-        if ($this.TotalCount -eq 0) { return 0 }
-        return $this.CurrentItem / $this.TotalCount * 100
+    [hashtable] GetProgress([string]$StatusTail) {
+        if ($this.AutoIncrement) { $this.Next() }
+        return @{
+            PercentComplete  = $this.PercentComplete
+            SecondsRemaining = $this.Remaining.TotalSeconds
+            Status           = "$($this.Status): $StatusTail"
+        }
     }
 
-    [timespan] Remaining() {
-        if ($this.TimeStopped) { return [timespan]::Zero }
-        if ($this.CurrentItem -eq 0) { return [timespan]::Zero }
-        $secondsPerItem = $this.Elapsed().TotalSeconds / $this.CurrentItem
-        $secondsRemaining = $secondsPerItem * ($this.TotalCount - $this.CurrentItem)
-        return [timespan]::FromSeconds($secondsRemaining)
-    }
-
-    [void] Next() {
+    hidden [void] Next() {
         $i = $this.CurrentItem + 1
-        $this | Add-Member -MemberType ScriptProperty -Name 'CurrentItem' -Value { return $i}.GetNewClosure() -Force
-    }
-
-    [string] Status() {
-        return "$($this.CurrentItem)/$($this.TotalCount)"
+        if ($i -gt $this.TotalCount) { $this.Stop() }
+        else { $this | Add-Member -MemberType ScriptProperty -Name 'CurrentItem' -Value { return $i }.GetNewClosure() -Force }
     }
 
     [void] Stop() {
@@ -60,7 +93,7 @@ function Get-MgGraphRequestAll {
     )
 
     $out = [psobject]@{
-         value = @()
+        value = @()
     }
 
     do {
@@ -79,25 +112,26 @@ function Get-MgGraphRequestAll {
 }
 
 # Connect to MS Graph, with scopes to read registration report and user authentication methods
-Connect-MgGraph -Scopes AuditLog.Read.All,UserAuthenticationMethod.Read.All -TenantId $TenantId -ContextScope Process -NoWelcome
+Connect-MgGraph -Scopes AuditLog.Read.All, UserAuthenticationMethod.Read.All -TenantId $TenantId -ContextScope Process -NoWelcome
 
 $reportUri = "https://graph.microsoft.com/beta/reports/authenticationMethods/userRegistrationDetails?`$filter=methodsRegistered/any(x:x eq 'microsoftAuthenticatorPush')&`$select=id,userPrincipalName,userDisplayName"
 $reportResult = Get-MgGraphRequestAll -Uri $reportUri
 
 $out = @()
-$t = [OperationTracker]::new($reportResult['Count'])
+$t = [OperationTracker]::new($reportResult['Count'],$true)
 foreach ($user in $reportResult.value) {
-    $t.Next()
-    Write-Progress -Activity "Getting app registration data for users" -Status "$($t.Status): ($($user.id)) $($user.userPrincipalName)" -id 2 `
-                   -PercentComplete $t.PercentComplete() -SecondsRemaining $t.Remaining().TotalSeconds
+    $progressSplat = $t.GetProgress("($($user.id)) $($user.userPrincipalName)")
+    Write-Progress -Activity "Getting app registration data for users" -Id 2 @progressSplat
     $userUri = "https://graph.microsoft.com/beta/users/$($user.id)/authentication/microsoftAuthenticatorMethods"
     $userResult = Invoke-MgGraphRequest -Method GET -Uri $userUri -OutputType PSObject
     $appGroup = $userResult.value.clientappName | Group-Object -AsHashTable
-    [array]$out += $user | Select-Object *, @{l='FullCount';e={$appGroup['microsoftAuthenticator'].count}}, @{l='LiteCount';e={$appGroup['outlookMobile'].count}}
+    [array]$out += $user | Select-Object *, @{l = 'FullCount'; e = { $appGroup['microsoftAuthenticator'].count } }, @{l = 'LiteCount'; e = { $appGroup['outlookMobile'].count } }
+    Remove-Variable userResult,appGroup,userUri,progressSplat -ErrorAction SilentlyContinue
 }
+
 $t.Stop()
-Write-Progress -id 2 -Activity "Getting app registration data for users" -PercentComplete 100 -Status "Done in $($t.Elapsed().ToString()), press any key to continue"
+Write-Progress -Id 2 -Activity "Getting app registration data for users" -PercentComplete 100 -Status "Done in $($t.Elapsed.ToString()), press any key to continue"
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-Write-Progress -id 2 -Completed
+Write-Progress -Id 2 -Completed
 
 $out | Export-Csv -Path .\AuthenticatorAppTypeReport.csv -NoTypeInformation
