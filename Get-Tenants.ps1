ac082eacb003aa3036f37a215169476979c93243
Pavel Garmashov <pgarm@pgarm.pro>
2023-12-12 11:45:59 +0000

    Update Get-Tenants.ps1
    Plain file only

--- Get-Tenants.ps1
+++ Get-Tenants.ps1
@@ -1,30 +1,19 @@
 #Requires -Version 7.0
 param (
-    [Parameter(Mandatory=$true,ParameterSetName='byFile')][ValidateScript({ Test-Path $_ -PathType Leaf <# Saves some time for later #>})][string]$Path,
-    [Parameter(Mandatory=$true,ParameterSetName='byList')][string[]]$TenantIds
+    [Parameter(Mandatory = $true)][ValidateScript({ Test-Path $_ -PathType Leaf })][string]$Path
 )
 
 # Connect to MsGraph - requires consent if not previously granted
 Connect-MgGraph -Scopes CrossTenantInformation.ReadBasic.All -ContextScope Process
 
 # Import the list of tenants if a file is specified (no processing if a list is provided)
-switch ($parameterSetName) {
-    'byFile' {
-        <#try {
-            $TenantIds = Import-Csv -Path $Path | Select-Object -ExpandProperty TenantId
-        }
-        catch {#>
-            $TenantIds = Get-Content -Path $Path
-        #}
-    }
-    Default {}
-}
+$TenantIds = Get-Content -Path $Path
 $out = @()
 
 # Get the tenant details
 foreach ($tenantId in $TenantIds) {
     $out += Invoke-MgGraphRequest -Method Get -Uri "beta/tenantRelationships/findTenantInformationByTenantId(tenantId='$tenantId')" -OutputType PSObject | 
-            Select-Object tenantId,displayName,defaultDomainName
+    Select-Object tenantId, displayName, defaultDomainName
 }
 
 # Output the results
